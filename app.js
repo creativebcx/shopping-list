6f57b66690f86eca49f16b26df7877d70f4127d8
creativebcx <ericdonley1@gmail.com>
2017-04-17 23:19:11 -0600

    not-functioning

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-Change main.js
+var list = {
+	items: []
+};
+//created object to for list array
+
+var addItem = function(list, item) {
+	list.items.push(item);
+};
+
+//created function to take array and add item to the list
+
+var renderList = function(list, element) {
+	var itemsHTML = list.items.map(function(item) {
+		return '<li>' + item + '</li>';
+	});
+	element.html(itemsHTML);
+};
+
+//created function to render HTML
+
+$('.shopping-list-entry').submit(function(event){
+	event.preventDefault();
+	addItem(list, $('.js-add-button').val());
+	renderList(list, $('.shopping-list'))
+	console.log("hello");
+})
