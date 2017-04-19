049ecdd901ad1a242f6b2c892d463bb4dbd79165
creativebcx <ericdonley1@gmail.com>
2017-04-18 22:11:16 -0600

    wonky-upload

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-var list = {
+var add = function(a, b) {
+	return a + b;
+}
+
+/*var list = {
 	items: []
-};
+};*/
 //created object to for list array
+//cannot get this empty object to work
+
+var list = ["apples", "pears"];
+var item = "milk";
+//testing variables
 
 var addItem = function(list, item) {
-	list.items.push(item);
+	list.push(item);
+	console.log(list);
 };
+//created function to add items to the array
 
-//created function to take array and add item to the list
-
-var renderList = function(list, element) {
-	var itemsHTML = list.items.map(function(item) {
-		return '<li>' + item + '</li>';
-	});
-	element.html(itemsHTML);
+var renderList = function(list) {
+	var itemsHTML = function(list) {
+		return '<li>' + list + '</li>';
+		console.log(itemsHTML);
+	};
+	//element.html(itemsHTML);
 };
-
 //created function to render HTML
 
 $('.shopping-list-entry').submit(function(event){
